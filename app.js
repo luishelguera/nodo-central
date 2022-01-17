e442f35679f6d6e4c12392f8ab7f83f3df2a03d6
Luis Helguera <luishelguera@objetiva.com.ar>
2022-01-17 16:54:04 -0300

    sadf

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 5000
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+// Templating Engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended : true }))
+
+// Routes
+const newsRouter = require('./src/routes/news')
+
+app.use('/', newsRouter)
+app.use('/article', newsRouter)
+
+// Listen on port 5000
+app.listen(port, () => console.log(`Listening on port ${port}`))
