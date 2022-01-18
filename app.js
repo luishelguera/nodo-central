777dec0b95afce4d1a32d8169c5eb542c8197b1c
luishelguera <luishelguera@objetiva.com.ar>
2022-01-18 18:16:15 -0300

    asdf

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.port
+const port = process.env.port || 80
 
 // Static Files
 app.use(express.static('public'))
